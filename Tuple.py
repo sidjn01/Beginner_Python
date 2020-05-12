4ec2033c6ee9a2287f729ef9f898fe28aafe9358
Sidhant Jain <64497367+sidjn01@users.noreply.github.com>
2020-05-12 21:12:32 +0530

    Add files via upload
    Tuples

---
+++ Tuple.py
@@ -0,0 +1,26 @@
+# t = "a", "b", "c"
+# print(t)
+#
+# print("a", "b", "c")
+# print(("a", "b", "c"))
+
+welcome = "Welcome to my nightmare", "Alice Cooper", 1975
+bad = "Bad Company " "Bad Company", 1974
+
+print(welcome)
+print(welcome[0])
+print(welcome[1])
+print(welcome[2])
+
+print(bad)
+
+metallica2 = ["Rid the Lightning", "Metallica", 1984]
+print(metallica2)
+metallica2[0] = "Master of Puppets"
+print(metallica2)
+
+title, artists, year = welcome
+print(title)
+print(artists)
+print(year)
+
---
+++ demo.py
@@ -0,0 +1,12 @@
+a = b = c = 12
+
+print(a, b, c)
+
+a, b = 12, 13
+
+print(a, b)
+
+a, b = b, a
+
+print("a is {}".format(a))
+print("b is {}".format(b))
