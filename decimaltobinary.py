50044f8e0d529b8a1e3e1d87067633b0a5ba54a4
Sidhant Jain <64497367+sidjn01@users.noreply.github.com>
2020-05-17 00:12:23 +0530

    Add files via upload

---
+++ decimaltobinary.py
@@ -0,0 +1,18 @@
+powers = []
+for power in range(15, -1, -1):
+    powers.append(2 ** power)
+
+print(powers)
+
+x = int(input("Please enter a number:"))
+
+printing = False
+ 
+for power in powers:
+    bit = x//power
+    if bit != 0:
+        printing = True
+    if printing:
+        print(bit, end='')
+    print(x // power, end='')
+    x %= power
